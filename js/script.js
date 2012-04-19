013e2595039f3defa08b0966fe81ed5ff5942785
Jean-Philippe Dery <jeanphilippe.dery@gmail.com>
2012-04-18 20:30:34 -0400

    Mobile fix

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@
 
 */
 
+(function(){
+
+Browser.Device = {
+	name: 'other'
+};
+
+if (Browser.Platform.ios){
+	var device = navigator.userAgent.toLowerCase().match(/(ip(ad|od|hone))/)[0];
+
+	Browser.Device[device] = true;
+	Browser.Device.name = device;
+}
+
+if (this.devicePixelRatio == 2)
+	Browser.hasHighResolution = true;
+
+Browser.isMobile = !['mac', 'linux', 'win'].contains(Browser.Platform.name);
+
+}).call(this);
 
 window.addEvent('domready', function() {
 
@@ -14,5 +33,13 @@ window.addEvent('domready', function() {
 			header.removeClass('emphasize');
 		}
 	});
+
+	if (Browser.isMobile) {
+		document.getElements('.mobile').setStyle('display', 'block');
+		document.getElements('.desktop').setStyle('display', 'none');
+	} else {
+		document.getElements('.mobile').setStyle('display', 'none');
+		document.getElements('.desktop').setStyle('display', 'block');
+	}
 
 });
